0981461d5e902948d0bca36963a6710dd6ae3018
student <student@gnu.ac.in>
2023-08-21 09:57:24 +0530

    completed

--- app/src/main/java/com/example/mad_practical5_21012011118/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical5_21012011118/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
     class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,34 @@ import android.widget.EditText
         browseBtn.setOnClickListener {
             openBrowser(browseEditTxt.text.toString())
         }
+
+        val openCallBtn:Button = findViewById(R.id.openCallButton)
+        val openCallEditTxt:EditText =findViewById(R.id.editTextPhone)
+
+        openCallBtn.setOnClickListener {
+            openCall(openCallEditTxt.text.toString())
+        }
+
+        val openCallLogBtn:Button = findViewById(R.id.callLogButton)
+        val openGalleryBtn:Button = findViewById(R.id.galleryButton)
+        val openCameraBtn:Button = findViewById(R.id.cameraButton)
+        val openAlarmBtn:Button = findViewById(R.id.alarmButton)
+
+        openCallLogBtn.setOnClickListener {
+            openCallLog()
+        }
+
+        openGalleryBtn.setOnClickListener {
+            openGallery()
+        }
+
+        openCameraBtn.setOnClickListener {
+            openCamera()
+        }
+
+        openAlarmBtn.setOnClickListener {
+            openAlarm()
+        }
     }
 
     fun openBrowser(url: String)
@@ -27,28 +60,39 @@ import android.widget.EditText
         }
     }
 
-    fun openCall(num: String)
+    fun openCall(no: String)
     {
-
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$no")).also {
+            startActivity(it)
+        }
     }
 
     fun openCallLog()
     {
-
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also {
+            startActivity(it)
+        }
     }
 
     fun openGallery()
     {
-
+        Intent(Intent.ACTION_VIEW).setType("image/*").also {
+            startActivity(it)
+        }
     }
 
     fun openCamera()
     {
-
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+            startActivity(it)
+        }
     }
 
     fun openAlarm()
     {
-
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also {
+            startActivity(it)
+        }
     }
 }
+
